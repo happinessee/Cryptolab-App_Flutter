e46f11d6eaf640130156295c8e771f5bf3868e69
happinessee <gygur1206@gmail.com>
2022-06-28 14:09:30 +0900

    ex02 column OK :D

--- week01/ex02/column/lib/main.dart
+++ week01/ex02/column/lib/main.dart
@@ -25,23 +25,36 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: false,
-        title: const Text(
-          '코동이',
-          style: TextStyle(color: Colors.black, fontFamily: 'summer'),
-        ),
-        backgroundColor: Colors.white,
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(
-              Icons.error,
-              color: Colors.black,
-            ),
-            onPressed: () => {},
+        appBar: AppBar(
+          centerTitle: false,
+          title: const Text(
+            '코동이',
+            style: TextStyle(color: Colors.black, fontFamily: 'summer'),
           ),
-        ],
-      ),
-    );
+          backgroundColor: Colors.white,
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.error,
+                color: Colors.black,
+              ),
+              onPressed: () => {},
+            ),
+          ],
+        ),
+        body: Column(children: [
+          Container(
+            color: Colors.red,
+            height: 70,
+          ),
+          Container(
+            color: Colors.green,
+            height: 70,
+          ),
+          Container(
+            color: Colors.blue,
+            height: 70,
+          ),
+        ]));
   }
 }
