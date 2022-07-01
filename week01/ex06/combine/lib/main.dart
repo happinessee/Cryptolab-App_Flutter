8b0d047005be86b9b18c484bebf54cc2c52c86ea
happinessee <gygur1206@gmail.com>
2022-07-01 13:59:53 +0900

    ex06 combine OK :D

---
+++ week01/ex06/combine/lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: '코동이'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: false,
+          title: const Text(
+            '코동이',
+            style: TextStyle(color: Colors.black, fontFamily: 'summer'),
+          ),
+          backgroundColor: Colors.white,
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.error,
+                color: Colors.black,
+              ),
+              onPressed: () => {},
+            ),
+          ],
+        ),
+        body: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Column(
+            children: [
+              Container(
+                color: Colors.red,
+                height: 70,
+                width: 70,
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.green,
+                height: 70,
+                width: 70,
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                color: Colors.blue,
+                height: 70,
+                width: 70,
+              ),
+            ],
+          ),
+        ]));
+  }
+}
