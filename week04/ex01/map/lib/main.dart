32f7bbe0cb969d69a7ec8673e8d6c99eb9d42775
happinessee <gygur1206@gmail.com>
2022-07-26 10:06:56 +0900

    ex02 map OK :D

--- week04/ex01/map/lib/main.dart
+++ week04/ex01/map/lib/main.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:flutter_config/flutter_config.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 import 'dart:ffi';
+import 'dart:async';
 
 void main() async {
+  // apiKey의 호출을 위함.
   WidgetsFlutterBinding.ensureInitialized();
   await FlutterConfig.loadEnvVariables();
   runApp(const MyApp());
@@ -13,7 +17,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,8 +35,14 @@ class Map extends StatefulWidget {
 
 class MapState extends State<Map> {
   var currentPosition;
-  var currentLocationAddress;
-  var apiKey = FlutterConfig.get('apiKey');
+  //var currentLocationAddress;
+  Completer<GoogleMapController> myController = Completer();
+
+  static CameraPosition kAnyang = const CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.4108359, 126.9123497),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
 
   @override
   initState() {
@@ -44,39 +53,21 @@ class MapState extends State<Map> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          if (currentPosition != null)
-            Text(
-              'LAT: ${currentPosition.latitude}',
-              style: TextStyle(fontSize: 23),
-            ),
-          SizedBox(height: 15),
-          if (currentPosition != null)
-            Text(
-              'LONG: ${currentPosition.longitude}',
-              style: TextStyle(fontSize: 23),
-            ),
-          SizedBox(height: 15),
-          if (currentLocationAddress != null)
-            Text(
-              currentLocationAddress,
-              style: TextStyle(fontSize: 23),
-            ),
-          SizedBox(height: 25),
-          ElevatedButton(
-            child: Text('Get Current Location',
-                style: TextStyle(fontSize: 22, color: Colors.white)),
-            onPressed: () {
-              getCurrentLocation();
-            },
-            style: ElevatedButton.styleFrom(primary: Colors.red),
-          ),
-        ],
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: kAnyang,
+        onMapCreated: (GoogleMapController controller) {
+          myController.complete(controller);
+        },
       ),
-    ));
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          changeCamera();
+        },
+        label: const Text('current My location'),
+        icon: const Icon(Icons.gps_fixed),
+      ),
+    );
   }
 
   getCurrentLocation() async {
@@ -87,25 +78,98 @@ class MapState extends State<Map> {
         .then((Position position) {
       setState(() {
         currentPosition = position;
-        getCurrentLocationAddress();
+        //getCurrentLocationAddress();
       });
     }).catchError((e) {
       print(e);
     });
   }
 
-  getCurrentLocationAddress() async {
-    try {
-      List<Placemark> listPlaceMarks = await placemarkFromCoordinates(
-          currentPosition.latitude, currentPosition.longitude);
-      Placemark place = listPlaceMarks[0];
-
-      setState(() {
-        currentLocationAddress =
-            '${place.locality}, ${place.postalCode}, ${place.country}';
-      });
-    } catch (e) {
-      print(e);
-    }
+  changeCamera() async {
+    getCurrentLocation();
+    GoogleMapController controller = await myController.future;
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(currentPosition.latitude, currentPosition.longitude),
+          zoom: 14.4746,
+        ),
+      ),
+    );
   }
+
+  //getCurrentLocationAddress() async {
+  //  try {
+  //    List<Placemark> listPlaceMarks = await placemarkFromCoordinates(
+  //        currentPosition.latitude, currentPosition.longitude);
+  //    Placemark place = listPlaceMarks[0];
+
+  //    setState(() {
+  //      currentLocationAddress =
+  //          '${place.locality}, ${place.postalCode}, ${place.country}';
+  //    });
+  //  } catch (e) {
+  //    print(e);
+  //  }
+  //}
 }
+
+//import 'dart:async';
+
+//import 'package:flutter/material.dart';
+//import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+//void main() => runApp(MyApp());
+
+//class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Flutter Google Maps Demo',
+//      home: MapSample(),
+//    );
+//  }
+//}
+
+//class MapSample extends StatefulWidget {
+//  @override
+//  State<MapSample> createState() => MapSampleState();
+//}
+
+//class MapSampleState extends State<MapSample> {
+//  Completer<GoogleMapController> _controller = Completer();
+
+//  static final CameraPosition _kGooglePlex = CameraPosition(
+//    target: LatLng(37.42796133580664, -122.085749655962),
+//    zoom: 14.4746,
+//  );
+
+//  static final CameraPosition _kLake = CameraPosition(
+//      bearing: 192.8334901395799,
+//      target: LatLng(37.43296265331129, -122.08832357078792),
+//      tilt: 59.440717697143555,
+//      zoom: 19.151926040649414);
+
+//  @override
+//  Widget build(BuildContext context) {
+//    return new Scaffold(
+//      body: GoogleMap(
+//        mapType: MapType.hybrid,
+//        initialCameraPosition: _kGooglePlex,
+//        onMapCreated: (GoogleMapController controller) {
+//          _controller.complete(controller);
+//        },
+//      ),
+//      floatingActionButton: FloatingActionButton.extended(
+//        onPressed: _goToTheLake,
+//        label: Text('To the lake!'),
+//        icon: Icon(Icons.directions_boat),
+//      ),
+//    );
+//  }
+
+//  Future<void> _goToTheLake() async {
+//    final GoogleMapController controller = await _controller.future;
+//    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+//  }
+//}
